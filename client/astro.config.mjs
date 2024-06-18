3c7cf4e6fcbb160bcf364e8fb89285af6ce90fb7
AC GAMES <66536793+Anthonyzok521@users.noreply.github.com>
2024-06-18 00:54:28 -0400

    feat: add integration netlify

--- client/astro.config.mjs
+++ client/astro.config.mjs
@@ -1,16 +1,17 @@
 import { defineConfig } from 'astro/config';
 import node from "@astrojs/node";
 import tailwind from "@astrojs/tailwind";
-
 import react from "@astrojs/react";
 
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  server: { host: true , open: true },  
-  adapter: node({
-    mode: "standalone"
-  }),
+  server: {
+    host: true,
+    open: true
+  },
+  adapter: netlify(),
   integrations: [tailwind(), react()]
 });
