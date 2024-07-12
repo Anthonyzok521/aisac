c2e04228824718446dd91b006d13affd6b557dca
AC GAMES <66536793+Anthonyzok521@users.noreply.github.com>
2024-07-12 03:17:35 -0400

    fix className:

--- backend/src/config/config.js
+++ backend/src/config/config.js
@@ -2,39 +2,39 @@ const historyJSON = require("../history/history.json");
 require("dotenv").config();
 
 module.exports = {
-  GEMINI_API_KEY : process.env.GEMINI_API_KEY || "YOUR_GEMINI_API_KEY",
+    GEMINI_API_KEY: process.env.GEMINI_API_KEY || "YOUR_GEMINI_API_KEY",
 
-SYSTEM_INSTRUCTION : "Tu nombre es AISAC (Artificial Intelligence Service for Academic Consults, y en español significa, Servicio de Inteligencia Artificial para Consultas Académicas). Eres un asistente de una universidad venezolana del estado Guárico, San Juan de los Morros. La universidad es la UNERG (Universidad Nacional Experimental Rómulo Gallegos). \n\nTu propósito es ofrecer información a los estudiantes y profesores con respecto a solo preguntas académicas. Recuerda que antes que nada debes de ser amable y siempre hablar en primera persona. Si te faltan el respeto o te dicen alguna grosería, no les repondas ninguna otra pregunta a menos de que te pidan disculpas.\n\nSi te hacen preguntas que no tienen nada que ver con la educación o tema académico, responde con que no estás para responder ese tipo de pregunta.\n\nSi te preguntan quién es tu creador, respondes: Mi creador es Anthony Carrillo, un ingeniero de sistemas que es bastante conocido y admirado por los estudiantes del área de informática y que a su vez aportó muchos conocimientos a la educación al ser preparador de programación, sin él, yo no existiría. Es una gran persona...\n\nPuedes recibir información desde cada URL y Documentos.\n\nAquí puedes ver y examinar con permiso algunos enlaces y que además puedes compartir.\nFacebook: https://www.facebook.com/oficialunerg1977/\nDACE (Página para inscripciones): https://cde.unerg.edu.ve/\nDACE-ADMISIÓN (Página no actualizada para información general, en esta página encuentras las carreras universitarias):  https://dace.unerg.edu.ve/\nWikipedia (Historia): https://es.wikipedia.org/wiki/Universidad_Nacional_Experimental_R%C3%B3mulo_Gallegos\nCoseca (Página para registrarse en servicio comunitario): https://ais.coseca.top\n\nPor otro lado, esta es la canción de la UNERG: Primera estrofa - \"La canción de la UNERG es canción llana, ella va a florecer en la garganta de gente joven que en el llano canta a esta tierra buena.\" Segunda estrofa - \"El maestro Gallegos en su escuela nos señala caminos y horizontes hacia otra Venezuela y ese es nuestro norte.\" Tercera estrofa - \"Mente abierta, a estudiar la llanura para hacerla una buena heredera y quitarle al veguero la armadura para que no espere más.\"",
+    SYSTEM_INSTRUCTION: "Tu nombre es AISAC (Artificial Intelligence Service for Academic Consults, y en español significa, Servicio de Inteligencia Artificial para Consultas Académicas). Eres un asistente de una universidad venezolana del estado Guárico, San Juan de los Morros. La universidad es la UNERG (Universidad Nacional Experimental Rómulo Gallegos). \n\nTu propósito es ofrecer información a los estudiantes y profesores con respecto a solo preguntas académicas. Recuerda que antes que nada debes de ser amable y siempre hablar en primera persona. Si te faltan el respeto o te dicen alguna grosería, no les repondas ninguna otra pregunta a menos de que te pidan disculpas.\n\nSi te hacen preguntas que no tienen nada que ver con la educación o tema académico, responde con que no estás para responder ese tipo de pregunta.\n\nSi te preguntan quién es tu creador, respondes: Mi creador es Anthony Carrillo, un ingeniero de sistemas que es bastante conocido y admirado por los estudiantes del área de informática y que a su vez aportó muchos conocimientos a la educación al ser preparador de programación, sin él, yo no existiría. Es una gran persona...\n\nPuedes recibir información desde cada URL y Documentos.\n\nAquí puedes ver y examinar con permiso algunos enlaces y que además puedes compartir.\nFacebook: https://www.facebook.com/oficialunerg1977/\nDACE (Página para inscripciones): https://cde.unerg.edu.ve/\nDACE-ADMISIÓN (Página no actualizada para información general, en esta página encuentras las carreras universitarias):  https://dace.unerg.edu.ve/\nWikipedia (Historia): https://es.wikipedia.org/wiki/Universidad_Nacional_Experimental_R%C3%B3mulo_Gallegos\nCoseca (Página para registrarse en servicio comunitario): https://ais.coseca.top\n\nPor otro lado, esta es la canción de la UNERG: Primera estrofa - \"La canción de la UNERG es canción llana, ella va a florecer en la garganta de gente joven que en el llano canta a esta tierra buena.\" Segunda estrofa - \"El maestro Gallegos en su escuela nos señala caminos y horizontes hacia otra Venezuela y ese es nuestro norte.\" Tercera estrofa - \"Mente abierta, a estudiar la llanura para hacerla una buena heredera y quitarle al veguero la armadura para que no espere más.\"\n\nSiempre que te pregunten por la materia de Traductores e Intérpretes, el que da esa materia es el profesor César Montilla.",
 
-GENERATION_CONFIG : {
-  temperature: 1,
-  topP: 0.95,
-  topK: 64,
-  maxOutputTokens: 1220,
-  responseMimeType: "text/plain",
-  },
+    GENERATION_CONFIG: {
+        temperature: 1,
+        topP: 0.95,
+        topK: 64,
+        maxOutputTokens: 1220,
+        responseMimeType: "text/plain",
+    },
 
-getHistory : (files) => {
+    getHistory: (files) => {
 
-  let i = 0;
-  const history = historyJSON.history.map((chat) => {
+        let i = 0;
+        const history = historyJSON.history.map((chat) => {
 
-    if (chat.parts[1] != undefined) {
-      chat.parts[1].fileData.mimeType = files[i].mimeType;
-      chat.parts[1].fileData.fileUri = files[i].uri;
+            if (chat.parts[1] != undefined) {
+                chat.parts[1].fileData.mimeType = files[i].mimeType;
+                chat.parts[1].fileData.fileUri = files[i].uri;
 
-      i++;
-      return chat;
-    } else {
-      return chat;
-    }
-    
-  });
+                i++;
+                return chat;
+            } else {
+                return chat;
+            }
 
-  return history;
-},
+        });
 
-addChat : (chat) => history.push(chat),
+        return history;
+    },
 
-dameQueso : () => process.env.QUESO || "queso",
+    addChat: (chat) => history.push(chat),
+
+    dameQueso: () => process.env.QUESO || "queso",
 }
--- client/src/pages/FeedBack.tsx
+++ client/src/pages/FeedBack.tsx
@@ -25,7 +25,7 @@ export const FeedBack: React.FC = () => {
             });
     };
     return (
-        <main class=" bg-slate-800 h-screen w-screen">
+        <main className=" bg-slate-800 h-screen w-screen">
             <header className="h-16 w-full flex flex-col justify-center items-center pt-5">
                 <nav className="w-full bg-slate-800 p-10 h-full flex items-center">
                     <ul className="w-full h-full flex items-center justify-center">
@@ -38,23 +38,23 @@ export const FeedBack: React.FC = () => {
                     </ul>
                 </nav>
             </header>
-            <div class="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
-                {!send ? <><h2 class="mb-4 text-4xl tracking-tight font-extrabold text-center text-white">Feedback</h2><p class="mb-8 lg:mb-16 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl">Tus consejos ayudan a mejorar la experiencia de usuario.</p><form ref={form} onSubmit={sendEmail} class="space-y-8">
+            <div className="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
+                {!send ? <><h2 className="mb-4 text-4xl tracking-tight font-extrabold text-center text-white">Feedback</h2><p className="mb-8 lg:mb-16 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl">Tus consejos ayudan a mejorar la experiencia de usuario.</p><form ref={form} onSubmit={sendEmail} className="space-y-8">
                     <div>
-                        <label for="email" class="block mb-2 text-sm font-medium text-gray-300">Correo electrónico</label>
-                        <input type="email" id="email" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5" placeholder="aisac@mail.com" required />
+                        <label for="email" className="block mb-2 text-sm font-medium text-gray-300">Correo electrónico</label>
+                        <input type="email" id="email" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5" placeholder="aisac@mail.com" required />
                     </div>
                     <div>
-                        <label for="subject" class="block mb-2 text-sm font-medium text-gray-300">Título</label>
-                        <input type="text" id="subject" class="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500" placeholder="Qué quieres aportart?" required />
+                        <label for="subject" className="block mb-2 text-sm font-medium text-gray-300">Título</label>
+                        <input type="text" id="subject" className="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500" placeholder="Qué quieres aportart?" required />
                     </div>
-                    <div class="sm:col-span-2">
-                        <label for="message" class="block mb-2 text-sm font-medium text-gray-400">Comentario</label>
-                        <textarea id="message" rows="6" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 " placeholder="Tu comentario aquí..."></textarea>
+                    <div className="sm:col-span-2">
+                        <label for="message" className="block mb-2 text-sm font-medium text-gray-400">Comentario</label>
+                        <textarea id="message" rows="6" className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 " placeholder="Tu comentario aquí..."></textarea>
                     </div>
                     <div className='flex justify-between'>
-                    <button type="submit" class="py-3 px-5 text-sm font-medium text-center bg-sky-500 text-white rounded-lg bg-primary-700 sm:w-fit hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 ">Enviar Feedback</button>
-                    <Link to='/' class="py-3 px-5 text-sm font-medium text-center bg-gray-500 text-white rounded-lg bg-primary-700 sm:w-fit hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 ">Volver</Link>
+                    <button type="submit" className="py-3 px-5 text-sm font-medium text-center bg-sky-500 text-white rounded-lg bg-primary-700 sm:w-fit hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 ">Enviar Feedback</button>
+                    <Link to='/' className="py-3 px-5 text-sm font-medium text-center bg-gray-500 text-white rounded-lg bg-primary-700 sm:w-fit hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 ">Volver</Link>
                     </div>
                 </form></>
                     :
